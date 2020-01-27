3c1cfbf654f9f87bef43d3f8a25bb776bcc73e07
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2020-01-27 18:32:15 -0300

    Correções e melhorias.

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -2,8 +2,12 @@ package br.com.alura.microservice.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableResourceServer
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
